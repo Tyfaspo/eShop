ff131f060b944e4e9e817beb8e7bfd7f50b9a9d0
Tyfaspo <svsh991@gmail.com>
2023-06-22 14:25:09 +0600

    pay action added

--- frontend/src/actions/orderActions.jsx
+++ frontend/src/actions/orderActions.jsx
@@ -7,6 +7,11 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_RESET,
 } from "../constants/orderConstants";
 
 import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
@@ -73,6 +78,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
+
     const { data } = await axios.get(
       `/api/orders/${id}/`,
       config
@@ -87,6 +93,46 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     console.log(error);
     dispatch({
       type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
+
+export const payOrder = (id, paymentResult) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_PAY_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/orders/${id}/pay/`,
+      paymentResult,
+      config
+    );
+
+    dispatch({
+      type: ORDER_PAY_SUCCESS,
+      payload: data,
+    });
+
+  } catch (error) {
+    console.log(error);
+    dispatch({
+      type: ORDER_PAY_FAIL,
       payload:
         error.response && error.response.data.detail
           ? error.response.data.detail
